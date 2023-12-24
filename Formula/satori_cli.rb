3ebdaf8484356eb533446704cd67ce7203a7b362
omer-shtivi <85064354+omer-shtivi@users.noreply.github.com>
2023-12-24 20:26:52 +0200

    satori_cli 0.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/satori_cli.rb
+++ Formula/satori_cli.rb
@@ -1,9 +1,9 @@
 class SatoriCli < Formula
     desc "Allow Satori users to obation database crendtials without the need to login to the Satori data portal"
     homepage "https://github.com/SatoriCyber/satori-cli" 
-    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.2/satori-0.0.2-macOS.tar.gz" 
-    sha256 "a9d44c6fd6fead46403424c499ad56a2e58bf6bb981f43849599cda660cf4588" 
-    version "0.0.2" 
+    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.3/satori-0.0.3-macOS.tar.gz" 
+    sha256 "4571663132cb68d97c2a081fe3b61396f13a085e2abde248dedea5c505128ef8" 
+    version "0.0.3" 
   
     def install
         bin.install "satori"
