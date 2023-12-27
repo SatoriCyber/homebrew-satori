4ab8a3c66215ae357144f512f3e00d694e3ea713
Omer Shtivi <omer.shtivi@satoricyber.com>
2023-12-27 22:27:42 +0200

    add support for auto complete

--- Formula/satori_cli.rb
+++ Formula/satori_cli.rb
@@ -1,12 +1,24 @@
 class SatoriCli < Formula
-    desc "Allow Satori users to obation database crendtials without the need to login to the Satori data portal"
-    homepage "https://github.com/SatoriCyber/satori-cli" 
-    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.4/satori-0.0.4-macOS.tar.gz" 
-    sha256 "9b62d8123c34bc086e2ad58e9b3ce9019517a158abc66fc0858cb0449efa5473" 
-    version "0.0.4" 
+    desc "Allow Satori users to obtain database credentials without the need to log in to the Satori data portal"
+    homepage "https://github.com/SatoriCyber/satori-cli"
+    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.4/satori-0.0.4-macOS.tar.gz"
+    sha256 "9b62d8123c34bc086e2ad58e9b3ce9019517a158abc66fc0858cb0449efa5473"
+    version "0.0.4"
   
     def install
-        bin.install "satori"
+      bin.install "satori"
+  
+      # Install the auto-complete script
+      bash_completion.d.install "satori_auto_complete.zsh"
+    end
+  
+    def caveats
+      <<~EOS
+        Bash completion has been installed.
+  
+        To enable auto-completion, add the following line to your shell configuration file (e.g., ~/.bashrc or ~/.zshrc):
+          source "$(brew --prefix)/etc/bash_completion.d/satori_auto_complete.zsh"
+      EOS
     end
   end
   
