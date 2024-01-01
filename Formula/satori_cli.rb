d29b584f60713589a701ee46fa564f04ef008634
omer-shtivi <85064354+omer-shtivi@users.noreply.github.com>
2024-01-01 19:24:51 +0200

    Revert "satori_cli 0.0.5 (#13)"
    This reverts commit df318b768c01ea6e5e6524a93712a05b05889731.

--- Formula/satori_cli.rb
+++ Formula/satori_cli.rb
@@ -1,9 +1,9 @@
 class SatoriCli < Formula
     desc "Allow Satori users to obtain database credentials without the need to log in to the Satori data portal"
     homepage "https://github.com/SatoriCyber/satori-cli"
-    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.5/satori-0.0.5-macOS.tar.gz"
-    sha256 "e7efc959c9718e0ef0de880b55feba6e893867db9d13a5a98b20bcd7adedba8a"
-    version "0.0.5"
+    url "https://github.com/SatoriCyber/satori-cli/releases/download/v0.0.4/satori-0.0.4-macOS.tar.gz"
+    sha256 "9b62d8123c34bc086e2ad58e9b3ce9019517a158abc66fc0858cb0449efa5473"
+    version "0.0.4"
   
     def install
       bin.install "satori"
